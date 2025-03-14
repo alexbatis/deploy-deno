c272e773b3ffe4bef342f2d91dc482c1f45f5392
Alex Batis <alexbatis412@gmail.com>
2025-03-14 09:49:36 -0500

    upgrade next to version 15.2.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div>Next.js on Deno Deploy</div>
+      <div>Next.js on Deno Deploy (next version 15.2.2)</div>
     </main>
   );
 }
